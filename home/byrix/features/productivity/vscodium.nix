d1cdd9d3857e91535947b4f5aff6e310f080eb6d
Byrix <git@byrix.dev>
2026-01-08 14:28:54 +1100

    vsc update

--- home/byrix/features/productivity/vscodium.nix
+++ home/byrix/features/productivity/vscodium.nix
@@ -1,1 +1,13 @@
-{ pkgs, ... }: { home.packages = [ pkgs.vscodium ]; }
+{ pkgs, ... }: {
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    profiles.default = {
+      enableUpdateCheck = false;
+      extensions = with pkgs.vscode-extensions; [
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+      ];
+    };
+  };
+}
