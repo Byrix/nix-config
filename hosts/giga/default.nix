d2b406d91fba6447cc3e24a5522d5971170c779a
Byrix <git@byrix.dev>
2026-01-16 00:36:12 +1000

    giga bootloader

--- hosts/giga/default.nix
+++ hosts/giga/default.nix
@@ -9,6 +9,8 @@
   networking.hostName = "giga";
   networking.networkmanager.enable = true;
 
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
   system.stateVersion = "25.05";
 }
 
