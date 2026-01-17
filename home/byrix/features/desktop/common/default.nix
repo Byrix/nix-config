c0b446bc5cf9a68e8acf02a79ee9b14e47335267
Byrix <git@byrix.dev>
2026-01-17 23:53:45 +0000

    init firefox

--- home/byrix/features/desktop/common/default.nix
+++ home/byrix/features/desktop/common/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./communication
     ./terminals/ghostty.nix
+    ./firefox.nix
   ];
 }
---
+++ home/byrix/features/desktop/common/firefox.nix
@@ -0,0 +1,32 @@
+{ config, ... }: {
+  programs.firefox = {
+    enable = true;
+    profiles.byrix = {
+      id = 0;
+      isDefault = true;
+      bookmarks = {};
+
+      search = {
+        default = "ddg";
+        privateDefault = "ddg";
+        force = true;
+        engines = {
+          nixos-wiki = {
+            name = "NixOS Wiki";
+            urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
+            iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
+            definedAliases = [ "@nw" ];
+          };
+          bing.metaData.hidden = true;
+          google.metaData.hidden = true;
+        };
+      };
+
+      # TODO: Extensions required 3rd party repo
+      # extensions = {
+      #   force = true;
+      #   packages = [];
+      # };
+    };
+  };
+}
