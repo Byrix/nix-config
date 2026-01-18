5b3c4429c473d4ee045d55e28f94a55d135f0854
Byrix <git@byrix.dev>
2026-01-18 00:25:08 +0000

    hyprland system level

---
+++ hosts/common/optional/desktops/hyprland.nix
@@ -0,0 +1,5 @@
+# NOTE: This module is required for hyprland, 
+# even if otherwise managed through home-man
+# See: https://wiki.hypr.land/Nix/Hyprland-on-NixOS/
+
+{ programs.hyprland.enable = true; }
