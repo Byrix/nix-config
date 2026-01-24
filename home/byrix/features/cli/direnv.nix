77130360457a346a99fda41ac1eae9a89e5592fa
Byrix <git@byrix.dev>
2026-01-24 18:53:45 +1100

    feat(tmux): init basic tmux setup

--- home/byrix/features/cli/direnv.nix
+++ home/byrix/features/cli/direnv.nix
@@ -6,18 +6,10 @@
       enable = true;
       silent = true;
       nix-direnv.enable = true;
-
-      # Disable default integrations in favour
-      # of direnv-instant instead
-      # See: https://github.com/Mic92/direnv-instant#setup
-      enableBashIntegration = false;
-      enableFishIntegration = false;
-      enableZshIntegration = false;
-      enableNushellIntegration = false;
     };
 
     direnv-instant = {
-      enable = true;
+      enable = config.programs.tmux.enable;
       enableBashIntegration = config.programs.bash.enable;
       enableFishIntegration = config.programs.fish.enable;
       enableZshIntegration = config.programs.zsh.enable;
---
+++ home/byrix/features/cli/tmux.nix
@@ -0,0 +1,9 @@
+{
+  programs.tmux = {
+    enable = true;
+
+    sensibleOnTop = true;
+    disableConfirmationPrompt = true;
+    newSession = true;
+  }
+}
