03a4984311969d7f023219567325026128bf9d0e
Byrix <git@byrix.dev>
2026-01-24 17:57:24 +1100

    feat(optimus): whitelisted direnv for homelab projects

--- home/byrix/optimus.nix
+++ home/byrix/optimus.nix
@@ -1,8 +1,14 @@
-{
+{ config, ... }: {
   imports = [
     ./global
     ./features/config/colours.nix
     ./features/desktop/common
     ./features/productivity
   ];
+
+  programs.direnv.config = {
+    whitelist.prefix = [
+      "${config.home.homeDirectory}/Documents/homelab"
+    ];
+  };
 }
