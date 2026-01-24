e86c31d31a3bd70fbbd0300e8a8d23a4ecb2075b
Byrix <git@byrix.dev>
2026-01-24 18:25:15 +1000

    feat(vscode): expanded list of default extensions

--- home/byrix/features/productivity/vscode/default.nix
+++ home/byrix/features/productivity/vscode/default.nix
@@ -7,9 +7,16 @@
       enableUpdateCheck = false;
 
       extensions = with pkgs.vscode-extensions; [
+        mkhl.direnv
+        vscodevim.vim
+        mikestead.dotenv
+        usernamehw.errorlens
         gruntfuggly.todo-tree
         gitlab.gitlab-workflow
-        mkhl.direnv
+        naumovs.color-highlight
+        aaron-bond.better-comments
+        alefragnani.project-manager
+        christian-kohler.path-intellisense
       ];
     };
   };
