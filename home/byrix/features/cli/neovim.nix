923a03eae82c07f6e1613d17ccd782dcd412c565
Byrix <git@byrix.dev>
2026-01-24 18:41:30 +1100

    fix(nvim): corrected typos and removed non-existent tree-sitter parser for zsh

--- home/byrix/features/cli/neovim.nix
+++ home/byrix/features/cli/neovim.nix
@@ -4,26 +4,28 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    vimDiffAlias = true;
+    vimdiffAlias = true;
 
     plugins = with pkgs.vimPlugins; [
-      nvim-treesitter
-      nvim-treesitter-context
       direnv-vim
 
-      # Treesitter Parsers
-      nvim-treesitter-parsers.bash
-      nvim-treesitter-parsers.zsh
-      nvim-treesitter-parsers.fish
-      nvim-treesitter-parsers.toml
-      nvim-treesitter-parsers.json
-      nvim-treesitter-parsers.yaml
-      nvim-treesitter-parsers.csv
-      nvim-treesitter-parsers.markdown
-      nvim-treesitter-parsers.markdown-inline
-      nvim-treesitter-parsers.regex
-      nvim-treesitter-parsers.gitignore
-      nvim-treesitter-parsers.gitcommit
+      # Treesitter
+      (nvim-treesitter.withPlugins (p: [
+        p.bash
+        # p.zsh
+        p.fish
+        p.toml
+        p.json
+        p.yaml
+        p.csv
+        p.markdown
+        p.markdown-inline
+        p.regex
+        p.gitignore
+        p.gitcommit
+      ]))
+      nvim-treesitter-context
+      nvim-treesitter-endwise
     ];
   }; 
 }
