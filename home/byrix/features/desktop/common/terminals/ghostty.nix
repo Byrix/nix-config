582b95b23e3a8b9450dd468788f571d4108bb04d
Byrix <git@byrix.dev>
2026-01-25 08:02:40 +1100

    feat(ghostty): integrate with font profiles

--- home/byrix/features/desktop/common/terminals/ghostty.nix
+++ home/byrix/features/desktop/common/terminals/ghostty.nix
@@ -1,8 +1,17 @@
-{
+{ config, ... }: {
   programs.ghostty = {
     enable = true;
-    enableBashIntegration = true;
-    enableFishIntegration = true;
-    enableZshIntegration = true;
+    # systemd = true;
+
+    settings = with config.fontProfiles; {
+      font-family = monospace.name;
+      font-style = "Regular";
+      font-style-bold = "Bold";
+      font-synthetic-style = "no-bold";
+      font-size = monospace.size;
+
+      background-opacity = 0.6;
+      background-blur = true;
+    };
   };
 }
