9b158ac23625f8d273c880cd3ece048269659a3d
Byrix <git@byrix.dev>
2026-01-29 16:22:52 +1000

    feat(waybar): init styling

--- home/byrix/features/desktop/common/waybar.nix
+++ home/byrix/features/desktop/common/waybar.nix
@@ -63,7 +63,7 @@
           nospacing = 1;
         };
         wireplumber = {
-          format = "{icon}";
+          format = "{icon} ";
           format-bluetooth = "󰂰";
           format-muted = "󰝟";
           format-icons = {
@@ -120,6 +120,100 @@
         };
       };
     };
+
+    style = with config.fontProfiles; ''
+       * {
+         border: none;
+         border-radius: 0;
+         min-height: 0;
+         font-family: ${monospace.name};
+         font-size: ${toString monospace.size}pt;
+       }
+
+      window#waybar {
+       background-color: @mantle;
+       transition-property: background-coolor;
+       transition-duration: 0.5s;
+      }
+      window#waybar.hidden { opacity: 0.5; }
+
+      #workspaces { background-color: transparent; }
+      #workspaces button {
+       all: initial;
+       min-width: 0;
+       box-shadow: inset 0 -3px transparent;
+       padding: 6px 18px;
+       margin: 6px 3px;
+       border-radius: 4px;
+       background-color: @base;
+       color: @text;
+      }
+      #workspaces button.active {
+        color: @mantle;
+        background-color: @text;
+      }
+      #workspaces button.hover {
+        box-shadow: inherit;
+        text-shadow: inherit;
+        color: @base;
+        background-color: @text;
+      }
+      #workspaces button.urgent { background-color: @red }
+      #workspaces button.empty { color: @surface1; }
+
+      #memory,
+      #custom-power,
+      #battery,
+      #backlight,
+      #wireplumber,
+      #network,
+      #clock,
+      #tray {
+        border-radius: 4px;
+        margin: 6px 3px;
+        padding: 6px 12px;
+        background-color: @base;
+        color: @mantle;
+      }
+
+      #memory { background-color: @peach; }
+      #backlight { background-color: @peach; }
+      #wireplumber { background-color: @yellow; }
+
+      #custom-power {
+        background-color: @flamingo;
+        margin-right: 6px;
+      }
+
+      #custom-logo {
+        padding: 0 7px 0 7px;
+        margin-left: 5px;
+        font-size: ${toString (monospace.size * 1.5)}pt;
+        border-radius: 8px 0 0 8px;
+        color: @sapphire;
+      }
+
+      #battery { background-color: @teal; }
+      #battery.warning,
+      #battery.critical,
+      #battery.urgent {
+        background-color: @red;
+        color: @mantle;
+      }
+      #battery.charging {
+        background-color: @green;
+        color: @mantle;
+      }
+
+      #network {
+        background-color: @teal;
+        padding-right: 17px;
+      }
+
+      #clock {
+        font-family: ${regular.name};
+        background-color: @mauve;
+      }
+    '';
   };
-  catppuccin.waybar.enable = false;
 }
