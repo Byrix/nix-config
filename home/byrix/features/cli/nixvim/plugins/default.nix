4742f203cc840ed220127451020f24f79c315e1d
Byrix <git@byrix.dev>
2026-02-01 00:18:18 +1000

    feat(nixvim): git-related plugin keybinds

--- home/byrix/features/cli/nixvim/plugins/default.nix
+++ home/byrix/features/cli/nixvim/plugins/default.nix
@@ -24,6 +24,7 @@
     diffview.enable = true;
     gitsigns.enable = true;
     git-conflict.enable = true;
+    gitblame.enable = true;
     lazygit.enable = true;
 
     # Visuals & UI
@@ -44,4 +45,27 @@
       };
     };
   };
+
+  keymaps =
+    let
+      nmap = k: a: d: {
+        key = k;
+        action = a;
+        mode = "n";
+        options = {
+          silent = true;
+          noremap = true;
+          desc = d;
+        };
+      };
+    in
+    [
+      # Git maps
+      (nmap "<leader>g" "" "[g]it")
+      (nmap "<leader>gg" "<cmd>LazyGit<cr>" "lazy[g]it")
+      (nmap "<leader>gd" "" "[d]iff")
+      (nmap "<leader>gdo" "<cmd>DiffviewOpen<cr>" "[o]pen")
+      (nmap "<leader>gdc" "<cmd>DiffviewClose<cr>" "[c]lose")
+      (nmap "<leader>gb" "<cmd>GitBlameToggle<cr>" "[b]lame")
+    ];
 }
