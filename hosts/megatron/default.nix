2c03a2279ffb8fe83bab7827e771af7565f79294
Byrix <git@byrix.dev>
2026-02-02 12:54:56 +1100

    fix(megatron): remove network settings
    remove declarative network settings, use dhcp instead
    defined settings were resulting in no network and a manual dis- and
    reconnect being required with networkmanager

--- hosts/megatron/default.nix
+++ hosts/megatron/default.nix
@@ -35,26 +35,7 @@
     };
   };
 
-  networking =
-    let
-      eth = "enp34s0";
-    in
-    {
-      hostName = "megatron";
-      defaultGateway = {
-        address = "192.168.1.1";
-        interface = eth;
-      };
-      interfaces."${eth}" = {
-        name = "eth-solaris";
-        ipv4.addresses = [
-          {
-            address = "192.168.1.10";
-            prefixLength = 24;
-          }
-        ];
-      };
-    };
+  networking.hostName = "megatron";
 
   programs.firefox.enable = true;
   hardware = {
